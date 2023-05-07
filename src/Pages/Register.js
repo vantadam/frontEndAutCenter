7bb6a19cd54206a2b3d942c20d46b166164753df
vantadam <adem.zaier.9@gmail.com>
2023-05-07 22:09:36 +0100

    registerUI

--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -1,10 +1,14 @@
-import "./Login.css"
+import "./Register.css"
 import { useNavigate } from "react-router-dom";
 import { useEffect,useState } from "react";
 import fb from "../Assets/fb.png";
 import google from "../Assets/google.png";
 
 function Register(){
+    const [email,setEmail]=useState("");
+const [pass,setPass]=useState("");
+const [name,setName]=useState("");
+const [lname,setLname]=useState("");
     const navigate=useNavigate();
 
     
@@ -12,10 +16,10 @@ return (
  <div className="loginPage">
  <div className="signINcontainer">
 <p className="cardTitle">Sign Up</p>
-<input type="fname" class="input-field" placeholder="Enter text"/>
-<input type="lname" class="input-field" placeholder="Enter text"/>
-<input type="email" class="input-field" placeholder="Enter text"/>
-<input type="password" class="input-field" placeholder="Enter text"/>
+<input type="text"  className='input' placeholder='First Name'onChange={val=>{setName(val.target.value)}}/>
+<input type="text"  className='input' placeholder='Last Name'onChange={val=>{setLname(val.target.value)}}/>
+<input type="email"  className='input' placeholder='Email'onChange={val=>{setEmail(val.target.value)}}/>
+<input type="password"  className='input' placeholder='Password'onChange={val=>{setPass(val.target.value)}}/>
 
 <div className="button" onClick={()=>alert("hello")}>SIGN UP</div>
 <p className="optionsText">Or login with</p>
